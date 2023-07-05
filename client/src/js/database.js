2f45f05493deb4f8a42f5512205ab6a1b1a84b7d
jkrieger6 <jkrieger11@gmail.com>
2023-07-05 10:33:34 -0700

    added logic to put method and get mthod for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put(content);
+  await tx.done;
+  console.log('putDb done');
+  db.close();
+}; 
+console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.done;
+  console.log('getDb done');
+  db.close();
+  return content;
+};
+console.error('getDb not implemented');
 
 initdb();
