9c97ceaca079a4b09f3a54ed40f76add90745070
jkrieger6 <jkrieger11@gmail.com>
2023-07-10 10:23:42 -0700

    fixed logo loading PUT and GET errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,23 +14,45 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>{
+  console.log('PUT to the database');
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put({ id: id, value: content });
-  await tx.done;
-  console.log('putDb done');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
 }; 
+
+// export const putDb = async (id, content) =>{
+//   const db = await openDB('jate', 1);
+//   const tx = db.transaction('jate', 'readwrite');
+//   const store = tx.objectStore('jate');
+//   await store.put({ id: id, value: content });
+//   await tx.done;
+//   console.log('putDb done');
+// }; 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const content = await store.getAll();
-  await tx.done;
-  console.log('getDb done');
-  return content;
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
 };
+
+// export const getDb = async () => {
+//   const db = await openDB('jate', 1);
+//   const tx = db.transaction('jate', 'readonly');
+//   const store = tx.objectStore('jate');
+//   const content = await store.getAll();
+//   await tx.done;
+//   console.log('getDb done');
+//   return content;
+// };
 
 initdb();
